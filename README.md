c5d72a0278db3de386a18b0855db8777b8d36bb0
janosspaits <140593800+janosspaits@users.noreply.github.com>
2023-08-22 12:35:00 +0100

    Added converter GUI buttons, input field, result

--- README.md
+++ README.md
@@ -3,5 +3,15 @@ currency converter with GUI
 
 
 tkinter documentation: https://docs.python.org/3/library/tkinter.html
+                        https://tkdocs.com/pyref/optionmenu.html
+                        http://tcl.tk/man/tcl8.6/TkCmd/entry.htm
 
 forex python documentaion: https://forex-python.readthedocs.io/en/latest/usage.html
+
+# Helpers
+
+c.get_symbol('GBP')
+
+c.get_symbol('EUR')
+
+c.get_rates("USD")
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ c = CurrencyRates()
 
 window = Tk()
 window.title("X-Change")
-window.minsize(500, 300)
+window.minsize(400, 300)
+window.resizable(False, False)
 # window.iconbitmap("icon path here")
 
 # some_day = datetime(2018, 9, 11)
@@ -17,9 +18,9 @@ window.minsize(500, 300)
 # amount = float(input("What is the amount?: "))
 
 
-def converter(from_curr, to_curr, money):
-    converted = c.convert(from_curr, to_curr, money)
-    return converted
+# def converter(from_curr, to_curr, money=1):
+#     converted = c.convert(from_curr, to_curr, money)
+#     return converted
 
 
 my_label = Label(text="Currency Converter", font=("Courier", 18, "bold"))
@@ -28,16 +29,57 @@ my_label.pack()
 currency_options = [
     "GBP",
     "USD",
+    "EUR",
+    "CHF",
+    "AUD",
+    "CAD",
     "HUF",
-    "EUR"
+    "JPY",
+    "CNY",
+    "HKD",
 ]
 
-clicked = StringVar()
-clicked.set("Choose a Currency")
-drop = OptionMenu(window, clicked, *currency_options)
-drop.pack()
+start_cur = StringVar()
+start_cur.set("Choose a Currency")
+start_drop = OptionMenu(window, start_cur, *currency_options)
+start_drop.configure(width=18)
+start_drop.pack(side="left")
 
-myButton = Button(window, text="What what", command="converter")
-myButton.pack()
+
+end_cur = StringVar()
+end_cur.set("Choose a Currency")
+end_drop = OptionMenu(window, end_cur, *currency_options)
+end_drop.configure(width=18)
+end_drop.pack(side="right")
+
+money_entered = Entry(width=10)
+money_entered.pack()
+
+
+def perform_conversion():
+    start_currency = start_cur.get()
+    end_currency = end_cur.get()
+    money = float(money_entered.get())
+
+    try:
+        result = c.get_rate(start_currency, end_currency, now) * money
+        # Update the label text with the result
+        result_label["text"] = f"{money} {start_currency} = {round(result, 3)} {end_currency}"
+    except Exception as e:
+        print(f"Error: {e}")
+        result_label["text"] = f"Error: {e}"
+
+
+# def on_click():
+#     result_label["text"] = "Python"
+#     convert_button["state"] = "disabled"
+
+
+convert_button = Button(window, text="Convert", command=perform_conversion, height=3, width=10)
+convert_button.pack(side="bottom")
+
+result_label = Label(text="Result", font=("Courier", 10, "bold"))
+result_label.pack(side="right")
 
 window.mainloop()
+
