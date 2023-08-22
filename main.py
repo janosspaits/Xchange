edb779db84c27e0687a3fd6639ee0437cf344238
Jani <jani.spaits@gmail.com>
2023-08-22 21:42:32 +0100

    GUI spacing improved, currency full names, added icon

--- main.py
+++ main.py
@@ -1,85 +1,71 @@
-from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyRates, CurrencyCodes
 from datetime import datetime
 from tkinter import *
 
 now = datetime.now()
 c = CurrencyRates()
+currency_codes = CurrencyCodes()
 
 window = Tk()
 window.title("X-Change")
-window.minsize(400, 300)
-window.resizable(False, False)
-# window.iconbitmap("icon path here")
+window.geometry("490x230")
+window.iconbitmap("dollarico.ico")
 
-# some_day = datetime(2018, 9, 11)
-# print(c.get_rate('GBP', 'HUF', some_day))
-# from_currency = input("Which currency would you like to exchange?: ")
-# to_currency = input("Which currency would you like to get?: ")
-# amount = float(input("What is the amount?: "))
+instructions = Label(text="Enter the amount:", font=("Arial", 12, "bold"))
+instructions.grid(row=0, column=1)
 
-
-# def converter(from_curr, to_curr, money=1):
-#     converted = c.convert(from_curr, to_curr, money)
-#     return converted
-
-
-my_label = Label(text="Currency Converter", font=("Courier", 18, "bold"))
-my_label.pack()
+to_label = Label(text="", font=("Arial", 10, "bold"))
+to_label.grid(row=2, column=1)
 
 currency_options = [
-    "GBP",
-    "USD",
-    "EUR",
-    "CHF",
-    "AUD",
-    "CAD",
-    "HUF",
-    "JPY",
-    "CNY",
-    "HKD",
+    f"{code} - \n{currency_codes.get_currency_name(code)}" for code in ["GBP", "USD", "EUR", 
+                                                                      "CHF", "AUD", "CAD", "HUF", 
+                                                                      "JPY", "CNY", "HKD"]
 ]
 
+money_entered = Entry(width=10)
+money_entered.grid(row=1, column=1, pady=5)
+
 start_cur = StringVar()
-start_cur.set("Choose a Currency")
+start_cur.set("Choose a Currency\n to convert from")
 start_drop = OptionMenu(window, start_cur, *currency_options)
 start_drop.configure(width=18)
-start_drop.pack(side="left")
-
+start_drop.grid(row=2, column=0, padx=10, pady=10)
 
 end_cur = StringVar()
-end_cur.set("Choose a Currency")
+end_cur.set("Choose a Currency\n to convert to")
 end_drop = OptionMenu(window, end_cur, *currency_options)
 end_drop.configure(width=18)
-end_drop.pack(side="right")
-
-money_entered = Entry(width=10)
-money_entered.pack()
+end_drop.grid(row=2, column=2, padx=1, pady=10)
 
 
 def perform_conversion():
-    start_currency = start_cur.get()
-    end_currency = end_cur.get()
-    money = float(money_entered.get())
+    start_currency = start_cur.get().split(' ')[0]
+    end_currency = end_cur.get().split(' ')[0]      
+    
+    if not money_entered.get().strip():
+        result_label["text"] = "Please enter an amount."
+        return
 
     try:
+        money = float(money_entered.get())
+    except ValueError:
+        result_label["text"] = "Invalid amount entered."
+        return
+    
+    try:
         result = c.get_rate(start_currency, end_currency, now) * money
-        # Update the label text with the result
         result_label["text"] = f"{money} {start_currency} = {round(result, 3)} {end_currency}"
     except Exception as e:
         print(f"Error: {e}")
         result_label["text"] = f"Error: {e}"
 
 
-# def on_click():
-#     result_label["text"] = "Python"
-#     convert_button["state"] = "disabled"
+convert_button = Button(window, text="Convert", command=perform_conversion, height=2, width=8)
+convert_button.grid(row=4, column=1, pady=10)
 
-
-convert_button = Button(window, text="Convert", command=perform_conversion, height=3, width=10)
-convert_button.pack(side="bottom")
-
-result_label = Label(text="Result", font=("Courier", 10, "bold"))
-result_label.pack(side="right")
+result_label = Label(text="Result", font=("Arial", 10, "bold"))
+result_label.grid(row=3, column=1, pady=15)
 
 window.mainloop()
 
